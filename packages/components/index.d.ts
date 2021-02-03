c70d651fc603dd40eda435da810fc4a8d1d62755
Semra Shukrieva <semra.shukrieva@gmail.com>
2021-02-03 11:16:41 +0000

    add type definitions for typescript usage

---
+++ packages/components/index.d.ts
@@ -0,0 +1,29 @@
+declare module '@digicat/components' {
+  interface ButtonProps {
+    children?: string
+    background?: string
+    size?: 'small' | 'medium' | 'large'
+    variant?: 'primary' | 'secondary' | 'outlined'
+  }
+
+  interface ImageType = {
+    
+  }
+
+  interface CarouselProps {
+    images: string[]
+      images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string
+    })
+  ).isRequired,
+  height: PropTypes.string,
+  width: PropTypes.string
+  }
+
+  
+
+  export const Button: (props: ButtonProps) => <JSXElement>
+  export const Carousel: (props: CarouselProps) => <JSXElement>
+}

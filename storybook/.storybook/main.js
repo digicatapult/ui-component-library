23f385d4db5a0ba86def9852aa0aaa0e74293ecf
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2022-12-05 14:56:40 +0000

    First commit

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,7 +1,19 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5'
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false
+      }
+    }
   }
-};
+}

f6e4e6dcf4cb80d26b9e84ca37aa46817df13704
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2022-12-05 16:46:23 +0000

    Add files

---
+++ packages/components/src/Button/Button.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { PropTypes } from 'prop-types'
+import styled from 'styled-components'
+
+import colors from '../colors'
+
+const { white, black, grey } = colors
+
+const StyledButton = styled.button`
+  border: ${({ variant }) =>
+    variant === 'outlined' ? `1px solid ${black}` : 'none'};
+  border-radius: 5px;
+  background-color: ${({ variant }) =>
+    (variant === 'outlined' && white) ||
+    (variant === 'text' && white) ||
+    black};
+  color: ${({ variant }) =>
+    variant === 'outlined' || variant === 'text' ? black : white};
+  text-transform: uppercase;
+  font-weight: 600;
+  min-width: ${({ variant }) => (variant === 'text' ? '10px' : '160px')};
+  height: 45px;
+  cursor: pointer;
+  &: disabled {
+    cursor: default;
+    background-color: ${({ variant }) =>
+      variant === 'outlined' || variant === 'text' ? 'white' : '#f0f0f0'} 
+    color: #6b7882;
+  }
+`
+
+const Button = ({ children, variant, ...rest }) => (
+  <StyledButton variant={variant} {...rest}>
+    {children}
+  </StyledButton>
+)
+
+Button.defaultProps = {
+  children: 'button',
+  variant: 'contained'
+}
+
+Button.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  variant: PropTypes.oneOf(['contained', 'outlined', 'text'])
+}
+
+export default Button
---
+++ storybook/stories/Button.stories.js
@@ -0,0 +1,31 @@
+import { Button } from '../../packages/components/src'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: { name: 'label', description: 'button text' },
+    style: {
+      name: 'style',
+      description:
+        'Style object will be passed as inline style as default behaviour of react elements. You can also use styled-components instead of this property which will also overwrite the default styles.'
+    },
+    variant: {
+      name: 'variant'
+    }
+  }
+}
+
+const Template = args => <Button {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {}
+
+export const Outlined = Template.bind({})
+Outlined.args = { variant: 'outlined' }
+
+export const Text = Template.bind({})
+Text.args = { variant: 'text' }
+
+export const Disabled = Template.bind({})
+Disabled.args = { disabled: true }

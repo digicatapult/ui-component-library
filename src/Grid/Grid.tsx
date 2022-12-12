1e1dbe8143d8ad521e7e3202281c728e63672f79
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-12-12 16:34:32 +0000

    Grid component
    Opinionated grid component that loosely wraps a css grid layout. Implemented grid and responsive layouts using a min-width media query. Error handling is pretty non-existant.

---
+++ src/Grid/Grid.tsx
@@ -0,0 +1,64 @@
+import React, { PropsWithChildren } from 'react'
+import styled from 'styled-components'
+
+import Panel, { PanelProps } from './GridPanel'
+
+export interface GridLayout {
+  areas: string[][]
+  rows: string[]
+  columns: string[]
+  gap?: string
+}
+
+export interface GridLayoutByWidth extends GridLayout {
+  minWidth: number
+}
+
+export interface GridProps extends GridLayout {
+  byWidth?: GridLayoutByWidth[]
+}
+
+export interface Grid extends React.FC<PropsWithChildren<GridProps>> {
+  Panel: React.FC<PropsWithChildren<PanelProps>>
+}
+
+const Grid: Grid = Object.assign(
+  (propsWithC: PropsWithChildren<GridProps>) => {
+    const { children, ...props } = propsWithC
+    return <Wrapper {...props}>{children}</Wrapper>
+  },
+  {
+    Panel,
+  }
+)
+
+Grid.Panel = Panel
+
+const interpolateLayout = ({ areas, rows, columns, gap }: GridLayout) => {
+  return `
+    grid-template-areas: ${areas.map((row) => `"${row.join(' ')}"`).join('\n')};
+    grid-template-rows: ${rows.join(' ')};
+    grid-template-columns: ${columns.join(' ')};
+    gap: ${gap || '0px'};
+  `
+}
+
+const Wrapper = styled('div')<GridProps>`
+  display: grid;
+  ${interpolateLayout}
+  ${({ byWidth }) => {
+    const entries = byWidth
+      ? byWidth.map(({ minWidth, ...props }) => {
+          return `
+        @media (min-width: ${minWidth}px) {
+          ${interpolateLayout(props)}
+        }
+      `
+        })
+      : []
+
+    return entries.join('\n')
+  }}
+`
+
+export default Grid

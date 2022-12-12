c16ddb778822db5cee63d5e7bdecc31a938b616e
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-12-12 11:53:07 +0000

    Fix eslint so it actually runs for the typescript parts of the codebase

--- src/stories/Card.stories.jsx
+++ src/stories/Card.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card } from '../index.js'
-import { Story } from "@storybook/react";
+import { Story } from '@storybook/react'
 
 export default {
   title: 'Card',

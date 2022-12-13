8f6bdc28b66b5b0c19013789e8ee7ed941343258
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-12-13 15:31:00 +0000

    Feature/drawer component (#25)
    * Drawer component
    
    * font inherit to fix cascade
    
    * Updates from review
    
    * Controls to default story and rename simple -> noparam
    
    * Add test for click

---
+++ src/Drawer/Drawer.stories.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Drawer } from '../index.js'
+import { Meta, Story } from '@storybook/react'
+import styled from 'styled-components'
+
+export default {
+  title: 'Components/Drawer',
+  component: Drawer,
+} as Meta<React.ComponentProps<typeof Drawer>>
+
+const Template: Story = (args) => {
+  return (
+    <StoryWrapper>
+      <Drawer
+        title={args.title}
+        width={args.width}
+        color={args.color}
+        background={args.background}
+      >
+        Test
+        <DrawerContentMock />
+        Test2
+        <DrawerContentMock />
+      </Drawer>
+      Some other content
+    </StoryWrapper>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  title: 'DRAWER',
+  width: '400px',
+  color: 'white',
+  background: '#7BB79B',
+}
+
+export const NoParams: Story = () => {
+  return (
+    <StoryWrapper>
+      <Drawer>
+        Test
+        <DrawerContentMock />
+        Test2
+        <DrawerContentMock />
+      </Drawer>
+      Some other content
+    </StoryWrapper>
+  )
+}
+
+export const Larger: Story = () => {
+  return (
+    <StoryLarge>
+      <Drawer
+        title="LARGE DRAWER"
+        width="600px"
+        color="white"
+        background="#7BB79B"
+      >
+        Test
+        <DrawerContentMock />
+        Test2
+        <DrawerContentMock />
+      </Drawer>
+      Some other content
+    </StoryLarge>
+  )
+}
+
+const StoryWrapper = styled.div`
+  font-family: Roboto;
+  font-size: 1em;
+  max-width: 500px;
+`
+
+const StoryLarge = styled.div`
+  font-family: Roboto;
+  font-size: 2em;
+`
+
+const DrawerContentMock = styled.div`
+  min-height: 200px;
+  background: magenta;
+`

badd7c5a7d7a473b342367f58e20f257b98e7f0b
Paulius <paulius.michelevicius@gmail.com>
2022-12-13 18:41:35 +0000

    HII-17: abstracting and sorting types, making easier to read.

--- src/AppBar/AppBar.tsx
+++ src/AppBar/AppBar.tsx
@@ -1,48 +1,52 @@
 import React, { PropsWithChildren } from 'react'
 import styled from 'styled-components'
 
-// TODO create css type, react might have it
+// TODO create css type, react might have it // break it down
 interface AppBarProps {
   fixed?: boolean
   shadow?: boolean
+  color?: string
   width?: string
   padding?: string
   direction?: string
   active?: boolean
   position?: string
   background?: string
-  theme?: object
-  children?: React.ReactNode
+  theme?:
+    | {
+        [key: string]: string
+      }
+    | undefined
 }
 
-export interface AppBar {
-  (args: AppBarProps): React.ReactElement
+interface AppBar {
+  (args: PropsWithChildren & AppBarProps): React.ReactElement
   Item?: React.FC<PropsWithChildren<AppBarProps>>
 }
 
-const Theme = {
-  primary: '#FFA',
-  accent: '#000',
+interface Item {
+  (args: PropsWithChildren<AppBarProps>): React.ReactElement
 }
 
 // TODO props per dom element as well as all for appbar
-const Wrapper = styled('div')<AppBarProps>`
+const Wrapper = styled('div')`
   display: flex;
-  width: ${({ width }) => width || '100%'};
+  width: ${({ width }: AppBarProps) => width || '100%'};
   flex-direction: row;
   justify-content: space-between;
   box-sizing: border-box;
   transition: box-shadow 1s cubic-bezier(0.8, 1, 0.2, 1) 3s;
-  box-shadow: ${({ shadow }) =>
-    shadow
+  box-shadow: ${({ shadow }: AppBarProps) =>
+    shadow || false
       ? '0px 3px 5px -2px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);'
       : 'none'};
-  background-color: ${({ color, theme }) => color || theme.primary};
-  color: ${({ theme }) => theme.accent || '#000'};
+  background-color: ${({ color, theme }: AppBarProps) =>
+    color || theme?.primary || '#FFFE'}
+  color: ${({ theme }: AppBarProps) => theme?.accent || '#000'};
 `
 
-const ToolBar = styled('ul')<AppBarProps>`
-  position: ${({ fixed }) => (fixed ? 'fixed' : 'relative')};
+const ToolBar = styled('ul')`
+  position: ${({ fixed }: AppBarProps) => (fixed ? 'fixed' : 'relative')};
   margin: 0;
   list-style-type: none;
   width: 100%;
@@ -52,12 +56,12 @@ const ToolBar = styled('ul')<AppBarProps>`
   height: 100%;
   min-height: 74px;
   gap: 15px;
-  flex-direction: ${({ direction }) => direction || 'row'};
-  padding: ${({ padding }) => padding || '0px 25px'};
+  flex-direction: ${({ direction }: AppBarProps) => direction || 'row'};
+  padding: ${({ padding }: AppBarProps) => padding || '0px 25px'};
   padding-left: 54px;
 `
 
-const Link = styled('a')<AppBarProps>`
+const Link = styled('a')`
   display: inline-block;
   text-align: center;
   text-decoration: none;
@@ -68,11 +72,11 @@ const Link = styled('a')<AppBarProps>`
   font-size: 1em;
   line-height: 74px;
 
-  ${({ active, theme }) =>
-    active ? `border-bottom: 2px solid ${theme.accent || '#000'}` : ``}
+  ${({ active, theme }: AppBarProps) =>
+    active ? `border-bottom: 2px solid ${theme?.accent || '#000'}` : ``}
 `
 
-const Li = styled('li')<AppBarProps>`
+const Li = styled('li')`
   display: inline-block;
   height: 100%;
   margin: 0;
@@ -85,32 +89,20 @@ const Li = styled('li')<AppBarProps>`
   }
 `
 
-const Item = ({
-  children,
-  ...props
-}: PropsWithChildren<AppBarProps>): JSX.Element => {
-  return (
-    <Li>
-      <Link href="#" {...props}>
-        {children}
-      </Link>
-    </Li>
-  )
-}
+const Item: Item = ({ children, ...props }) => (
+  <Li>
+    <Link href="#" {...props}>
+      {children}
+    </Link>
+  </Li>
+)
 
 // TODO update to use grid comopnent
-const AppBar: AppBar = ({
-  children,
-  shadow = false,
-  theme = Theme,
-  ...props
-}) => {
-  return (
-    <Wrapper {...props} theme={theme} shadow={shadow}>
-      <ToolBar>{children}</ToolBar>
-    </Wrapper>
-  )
-}
+const AppBar: AppBar = ({ children, ...props }) => (
+  <Wrapper {...props}>
+    <ToolBar>{children}</ToolBar>
+  </Wrapper>
+)
 
 AppBar.Item = Item
 

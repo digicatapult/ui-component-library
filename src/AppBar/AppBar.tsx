01c5c892350e8786b0f5537a3ae55385c1acb892
Paulius <paulius.michelevicius@gmail.com>
2022-12-13 12:27:51 +0000

    Hii-17; linting

--- src/AppBar/AppBar.tsx
+++ src/AppBar/AppBar.tsx
@@ -21,7 +21,10 @@ const Wrapper = styled('div')<AppBarProps>`
   justify-content: space-between;
   box-sizing: border-box;
   transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  box-shadow: ${({ shadow }) => shadow ? '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);' : 'none'};
+  box-shadow: ${({ shadow }) =>
+    shadow
+      ? '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);'
+      : 'none'};
   background-image: linear-gradient(
     rgba(255, 255, 255, 0.09),
     rgba(255, 255, 255, 0.09)
@@ -44,7 +47,7 @@ const Bar = styled('div')<AppBarProps>`
   height: 100%;
   min-height: 25px;
   gap: 5px;
-  background-color: ${({ background }) => background || 'none'};
+  background-color: ${({ background }) => background || '#d3d3d3'};
   flex-direction: ${({ direction }) => direction || 'row'};
   padding: ${({ padding }) => padding || '10px 25px'};
 `
@@ -52,14 +55,16 @@ const Bar = styled('div')<AppBarProps>`
 // TODO add support for logo icon? - not needed for this project
 const AppBar: React.FC<PropsWithChildren<AppBarProps>> = ({
   children,
-  search = true,
-  shadow = true,
+  search = false,
+  shadow = false,
   ...props
 }: PropsWithChildren<AppBarProps>) => {
-  return <Wrapper {...props} shadow={shadow}>
-    <Bar>{children}</Bar>
-    {search && <Search {...props} />}
-  </Wrapper>
+  return (
+    <Wrapper {...props} shadow={shadow}>
+      <Bar>{children}</Bar>
+      {search && <Search {...props} />}
+    </Wrapper>
+  )
 }
 
 export default AppBar

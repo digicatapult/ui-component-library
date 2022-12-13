5c385d65df9dba2d7945386067db5c4d99aae85f
Paulius <paulius.michelevicius@gmail.com>
2022-12-13 14:31:02 +0000

    HII-17: depcheck.

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,6 +4,7 @@ import packageJson from '../package.json'
 export default create({
   brandTitle: `
     <div style="display: flex; gap: 4px; align-items: center;">
+      <span>Digital Catapult</span><br />
       <span>${packageJson.name}@${packageJson.version}</span>
     </div>
   `

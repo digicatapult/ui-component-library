bda11dd453c2e6665b1b90afb955381f0b1ff3b0
Jonathan Gray <jonmattgray@gmail.com>
2022-12-19 13:31:53 +0000

    mock mapboxgl

--- src/Map/Map.spec.js
+++ src/Map/Map.spec.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import renderer, { act } from 'react-test-renderer'
+import mapboxgl from 'mapbox-gl'
+
 import 'jest-styled-components'
 
 import Map from './index.tsx'
 
+jest.mock('mapbox-gl')
+
+const mockMapOn = jest.fn()
+
+mapboxgl.Map.mockImplementation(() => {
+  return { on: mockMapOn }
+})
+
 describe('Map', () => {
-  test('simple', () => {
-    const tree = renderer
-      .create(<Map token={process.env.STORYBOOK_MAPBOX_TOKEN} />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+  test('simple', async () => {
+    let instance
+    await act(() => {
+      instance = renderer.create(<Map />)
+    })
+
+    expect(mockMapOn).toHaveBeenCalledTimes(1)
+    expect(instance.toJSON()).toMatchSnapshot()
   })
 })

9a151e800d5cf4cb8f89f1a3729ebd429992ce9f
Paulius <paulius.michelevicius@gmail.com>
2022-12-19 14:04:18 +0000

    HII-34: a couple of versions of dropdown that returns value in update fn."

---
+++ src/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Dropdown from './index'
+import { Story } from '@storybook/react'
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+  argTypes: {
+    isMulti : {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  args: {
+    options: [
+        {
+          value: '2',
+          label: 'option 1',
+          color: 'rgba(128, 204, 114, 0.6)',
+          textColor: 'white'
+        },
+        {
+          value: '1',
+          label: 'option 2',
+          color: 'rgba(182, 239, 160, 0.6)',
+        },
+        {
+          value: '3',
+          label: 'option 3',
+          color: 'rgba(223, 230, 103, 0.6)',
+        },
+        {
+          value: '4',
+          label: 'option 4',
+          color: 'rgba(223, 230, 103, 0.6)',
+        },
+      ]
+      
+  }
+}
+
+export const Default: Story<typeof Dropdown> = (args) => {
+  const [selected, setSelected] = React.useState('')
+
+  return (
+    <>
+        <h1>{JSON.stringify(selected)}</h1>
+        <Dropdown isMulti={false} update={(val: any) => setSelected(val)} {...args} />
+    </>
+  )
+}
+
+
+export const Multi: Story<typeof Dropdown> = (args) => {
+    const [selected, setSelected] = React.useState([])
+  
+    return (
+      <>
+          <h1>{selected.map((el) => JSON.stringify(el))}</h1>
+          <Dropdown isMulti={true} update={(val: any) => {
+            console.log(val)
+            setSelected(val)
+          }} {...args} />
+      </>
+    )
+  }
---
+++ src/Dropdown/index.tsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import Select, { StylesConfig }from 'react-select'
+import styled from 'styled-components'
+
+const Wrapper = styled('div')`
+  ${({ width }: { width?: string }) => `
+    width: ${width || '50%'}
+  `}
+`
+
+const ValuesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const Value = styled('div')`
+  padding: 0.5rem;
+  padding-left: 0.75rem;
+  margin: 0.5rem;
+  font-size: 1rem;
+  border-radius: 10px 0px 0px 10px;
+  color: ${(props: any) => props.textColor || '#216968'};
+  background-color: ${(props) => props.color || 'rgba(247, 173, 46, 0.6)'};
+  user-select: none;
+`;
+
+const X = styled('button')`
+  all: unset;
+  margin-left: 1.3rem;
+  color: black;
+  transition: fill 0.15s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    color: #bb392d;
+  }
+  &:focus {
+    color: #c82f21;
+  }
+`;
+
+interface Props {
+  options?: Array<any>
+  styles?: StylesConfig
+  isMulti: boolean
+  update: (val: any[]) => void
+}
+
+interface IDropdown {
+  (args: Props): React.ReactElement
+}
+
+
+const MultiSelect = (props: any) => {
+  const { value, onChange } = props;
+
+  const handleRemoveValue = (e: any) => {
+    if (!onChange) return;
+    const { name: buttonName } = e.currentTarget;
+    const removedValue = value.find((val: any) => val.value === buttonName);
+    if (!removedValue) return;
+    props.update(value)
+    onChange(
+      value.filter((val: any) => val.value !== buttonName),
+      { name, action: 'remove-value', removedValue }
+    );
+  };
+
+  return (
+    <Wrapper>
+      <Select
+        isMulti
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+        })}
+        {...props}
+        closeMenuOnSelect={false}
+        controlShouldRenderValue={true}
+      />
+      <ValuesContainer> {value.map((val: any) => (
+        <Value {...val} key={val.value}>
+          {val.label}
+          <X name={val.value} onClick={handleRemoveValue}>✕</X>
+        </Value>))}
+      </ValuesContainer>
+
+    </Wrapper>
+  );
+};
+
+
+const Dropdown: IDropdown= ({
+  options,
+  isMulti = false,
+  styles = {
+    multiValue: () => ({
+      display: 'none'
+    }),
+    option: (styles) => ({
+      ...styles,
+      ':hover': {
+        ...styles[':hover'],
+        backgroundColor: '#B6EFA0',
+        color: '#27847A',
+      },
+    }),
+    menuList: (styles) => ({
+      ...styles,
+      color: '#fff',
+      backgroundColor: '#27847A',
+    }),
+  },
+  ...props
+}) => {
+  const [value, setValue] = React.useState([]);
+
+  const update = (val: any) => props.update(val)
+  const onChange = (val: any) => {
+    update(val)
+    setValue(val)
+  }
+
+  // TODO show selected in drop down as well.
+  if (isMulti) return (
+    <MultiSelect
+      { ...{
+        options,
+        styles,
+        ...props,
+        onChange,
+        value
+      } }
+    />
+  )
+  return (
+    <Wrapper width={'50%'}>
+      <Select
+        options={options}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+        })}
+        onChange={onChange}
+        isMulti={isMulti}
+        closeMenuOnSelect={true}
+        styles={styles}
+      />
+    </Wrapper>
+  )
+}
+
+export default Dropdown

72c91612e67f8c18a60ac0d03a419b3d472b0f98
Jonathan Gray <jonmattgray@gmail.com>
2023-01-09 14:32:49 +0000

    overflow auto

--- src/Dialog/index.tsx
+++ src/Dialog/index.tsx
@@ -74,7 +74,7 @@ const Wrapper = styled.dialog<DialogProps>`
 const DialogForm = styled.form`
   display: inline-block;
   position: relative;
-  overflow: scroll;
+  overflow: auto;
   width: 100%;
   height: 100%;
 `

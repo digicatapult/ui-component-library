dadab3926e816a1fadcb29642889d6ad77bca52e
Paulius <paulius.michelevicius@gmail.com>
2023-10-19 12:00:56 +0100

    STP-84: a few little changes to the storybook docs.

--- src/FormGroup/FormGroup.stories.tsx
+++ src/FormGroup/FormGroup.stories.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-import FormGroup, { FormGroupProps } from './index.js'
+import { action } from '@storybook/addon-actions'
+import FormGroup from './index.js'
 import InputText from '../Input/index.js'
 
 export default {
   title: 'Components/FormGroup',
   component: FormGroup,
   argTypes: {
-    onSubmit: {},
-  },
+    onSubmit: { action: 'clicked' }
+  }
 }
 
-const DefaultStoryTemplate = (args: FormGroupProps) => (
-  <FormGroup onSubmit={args.onSubmit}>
+const DefaultStoryTemplate = () => (
+  <FormGroup onSubmit={action('submit')}>
     <InputText type="text" name="test-a" />
     <br />
     <button type="submit">submit</button>
@@ -20,10 +21,3 @@ const DefaultStoryTemplate = (args: FormGroupProps) => (
 )
 
 export const Default = DefaultStoryTemplate.bind({})
-
-Default.args = {
-  onSubmit: (e) => {
-    e.preventDefault()
-    window.alert(`submitted: ${e.target}`)
-  },
-}

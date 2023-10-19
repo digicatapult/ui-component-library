cfc0149b8267e1b5d3ada269f9c336fe90bcabc3
Paulius <paulius.michelevicius@gmail.com>
2023-10-19 12:18:07 +0100

    STP-84: oops, e.preventdefault.

--- src/FormGroup/FormGroup.stories.tsx
+++ src/FormGroup/FormGroup.stories.tsx
@@ -13,7 +13,10 @@ export default {
 }
 
 const DefaultStoryTemplate = () => (
-  <FormGroup onSubmit={action('submit')}>
+  <FormGroup onSubmit={(e) => {
+    e.preventDefault()
+    action('submit')(e.target)
+  }}}
     <InputText type="text" name="test-a" />
     <br />
     <button type="submit">submit</button>

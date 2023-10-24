6c718367a133a43472e5125c11ad99f8c2ef855a
Paulius <paulius.michelevicius@gmail.com>
2023-10-24 04:10:48 +0100

    STP-130: include new button in the form group story.

--- src/FormGroup/FormGroup.stories.tsx
+++ src/FormGroup/FormGroup.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { action } from '@storybook/addon-actions'
 import FormGroup from './index.js'
 import InputText from '../Input/index.js'
+import Button from '../Button/index.js'
 
 export default {
   title: 'Components/FormGroup',
@@ -14,14 +15,20 @@ export default {
 
 const DefaultStoryTemplate = () => (
   <FormGroup
-    onSubmit={(e) => {
+    onSubmit={(e: React.FormEvent) => {
       e.preventDefault()
-      action('submit')(e.target)
+      action('submit')(
+        Object.fromEntries(new FormData(e.target as HTMLFormElement)),
+      )
     }}
   >
     <InputText type="text" name="test-a" />
     <br />
-    <button type="submit">submit</button>
+    <InputText type="text" name="test-b" />
+    <br />
+    <InputText type="hidden" name="test-c" value="some hidden value" />
+    <br />
+    <Button type="submit">submit</Button>
   </FormGroup>
 )
 

ab71055009844aaf68c49c5ec9324956f24d0d8c
Paulius <paulius.michelevicius@gmail.com>
2024-01-18 08:54:52 +0000

    HYP-92: icon as react comp.

---
+++ src/Icons/Checked.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function(): React.ReactElement { 
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="13" viewBox="0 0 17 13" fill="none">
+      <path d="M1 5.5L6 10.5L15.5 1" stroke="green" stroke-width="2.5"/>
+    </svg>
+  )
+}

58fe23d67cb310e22d68aedfa0ea6a5d1604eb8d
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-01-24 10:23:32 +0000

    HYP-92: adding basic spinner.

---
+++ src/Animations/Spinner/Spinner.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+
+import Spinner from './index.js'
+
+describe('Spinner', () => {
+  test('default', () => {
+    const instance = renderer.create(<Spinner />)
+    const tree = instance.toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('with color and text props', () => {
+    const instance = renderer.create(<Spinner color="#ff00ff" text="this is a test" />)
+    const tree = instance.toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('renders a small spinner', () => {
+    const instance = renderer.create(<Spinner size='small' />)
+
+    const tree = instance.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('renders a medium spinner', () => {
+    const instance = renderer.create(<Spinner size='medium' />)
+
+    const tree = instance.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('renders a large spinner', () => {
+    const instance = renderer.create(<Spinner size='large' />)
+
+    const tree = instance.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/Animations/Spinner/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export type SpinnerProps = {
+  styles?: React.CSSProperties
+  text?: string
+  color?: string
+  size?: 'small' | 'medium' | 'large' | undefined
+}
+
+const sizeToPixel = {
+  small: '10px',
+  medium: '30px',
+  large: '50px',
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ text, ...props }) => {
+  return (
+      <Wrapper>
+        <Animation {...props} />
+        <Label>{text}</Label>
+      </Wrapper>
+  )
+}
+
+const Animation = styled('div')`
+  border: 10px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 10px solid ${(props: SpinnerProps) => props.color || '#3498db' };
+  width: ${(props: SpinnerProps) => sizeToPixel[props.size || 'small']};
+  height: ${(props: SpinnerProps) => sizeToPixel[props.size || 'small' ]};
+  margin: auto;
+  padding: auto;
+  -webkit-animation: spin 1.75s linear infinite; /* Safari */
+  animation: spin 1.75s linear infinite;
+  }
+
+  /* Safari */
+  @-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+  }
+
+  @keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+  }
+`
+const Wrapper = styled.div`
+  align-self: center;
+  justify-self: center;
+`
+
+const Label = styled.h2`
+  text-align: center;
+`
+
+export default Spinner

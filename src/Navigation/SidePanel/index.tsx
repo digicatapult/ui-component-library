f1bd12dd87437ed8f5fbad9a1c1c4a96ba7b7770
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-02-02 13:55:09 +0000

    HYP-111: adding a callback pt2 (clean up after testing)} }

--- src/Navigation/SidePanel/index.tsx
+++ src/Navigation/SidePanel/index.tsx
@@ -6,12 +6,12 @@ import { SideArrowCloseIcon, SideArrowOpenIcon } from '../../index.js'
 import Avatar from '../../UserIcon/index.js'
 
 export interface SidePanelProps {
-  callback: (data: { [k: string]: string | number | boolean }) => void 
   heading?: string
   title?: string
   variant?: 'default' | 'hyproof'
   width?: string
   isOpen?: boolean
+  callback?: (data: { [k: string]: string | number | boolean }) => void
   update?: (name: string, persona: Persona, e?: MouseEvent) => void
 }
 
@@ -58,16 +58,25 @@ const Item: React.FC<React.PropsWithChildren<SidePanelItemProps>> = ({
   </ItemWrapper>
 )
 
-const SidePanel: ISidePanel & IItem = ({ children, update, callback, heading, ...props }) => {
+const SidePanel: ISidePanel & IItem = ({
+  children,
+  update,
+  callback,
+  heading,
+  ...props
+}) => {
   const [show, setShow] = React.useState(false)
 
   const style = { width: props.width, isOpen: show } as React.CSSProperties
   return (
-    <Panel onAnimationEnd={() => {
-      setShow(true)
-      return callback(props)
-    }} style={style}>
-      <Button onClick={() => setShow(!show)} style={style}>
+    <Panel onAnimationEnd={() => setShow(true)} style={style}>
+      <Button
+        onClick={() => {
+          setShow(!show)
+          if (callback) return callback({ ...props, isOpen: !show })
+        }}
+        style={style}
+      >
         {show ? <SideArrowOpenIcon /> : <SideArrowCloseIcon />}
       </Button>
       <Heading>{`${heading} (${props.title})`}</Heading>

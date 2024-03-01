cfc338381581140acadada56f63fcf8105ab18e1
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-03-01 09:05:40 +0000

    HYP-133: Icons added.

--- src/Table/Table.stories.tsx
+++ src/Table/Table.stories.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 
+import PadlockIcon from '../Icons/Padlock.js'
+import ClockIcon from '../Icons/Clock.js'
+import CertificateIssueIcon from '../Icons/Certificate-Issued.js'
+import CertificateRevokedIcon from '../Icons/Certificate-Revoked.js'
+import CertificateCO2Icon from '../Icons/Certificate-CO2.js'
 import Table, { TableProps } from './index.js'
 
 export default {
@@ -11,15 +17,135 @@ export default {
   },
 }
 
+const Row = styled('div')`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  align-items: center;
+`
+
+const Col = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Title = styled('div')`
+  color: #6aa685;
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 0px; /* 0% */
+`
+
+const Status = styled('div')`
+  width: 90px;
+  background-color: ${(props) => props.color || '#27847a'};
+  color: #fff;
+  border-radius: 60px;
+
+  text-align: center;
+  font-family: Roboto;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 10px; /* 83.333% */
+`
+
+const Date = styled('div')`
+  color: #27847a;
+
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 10px; /* 83.333% */
+`
+
+const NameCell = ({ name, date }) => (
+  <Col>
+    <Row>
+      <PadlockIcon />
+      <Title>{name}</Title>
+    </Row>
+    <Row>
+      <ClockIcon />
+      <Date>{date}</Date>
+    </Row>
+  </Col>
+)
+
+const StatusCell = ({ color = '#27847a', Icon, status }) => (
+  <Col>
+    <Icon />
+    <Status color={color}>{status}</Status>
+  </Col>
+)
+
 const DefaultStoryTemplate = (args: TableProps) => <Table {...args} />
 
-export const Default = DefaultStoryTemplate.bind({})
+export const Hyproof = DefaultStoryTemplate.bind({})
 
-Default.args = {
-  headers: ['Timestamp', 'Subject', 'HTML'],
+Hyproof.args = {
+  headers: [
+    'Date',
+    'H2 Batch size',
+    'Electric energy use',
+    'Electric energy use',
+    'Status',
+  ],
   rows: [
-    ['2010-01-01T00:00:00.000Z', 'example-1', <p>paragraph</p>],
-    ['2011-01-01T00:00:00.000Z', 'example-2', <button>button</button>],
-    ['2012-01-01T00:00:00.000Z', 'example-3', <h3>heading #3</h3>],
+    [
+      <NameCell name={'UK-HYPROOF-0001'} date={'2021/01/12, 22:50h'} />,
+      '1.0 MWh',
+      '4.2 MWh',
+      '45g CO2e',
+      <StatusCell Icon={CertificateIssueIcon} status={'issued'} />,
+    ],
+    [
+      <NameCell name={'UK-HYPROOF-0002'} date={'2024/02/22, 22:50h'} />,
+      '0.5 MWh',
+      '4.2 MWh',
+      '45g CO2e',
+      <StatusCell Icon={CertificateCO2Icon} status={'C.Embodiment'} />,
+    ],
+    [
+      <NameCell name={'UK-HYPROOF-0003'} date={'2024/02/02, 22:50h'} />,
+      '2.0 MWh',
+      '4.2 MWh',
+      '45g CO2e',
+      <StatusCell Icon={CertificateCO2Icon} status={'C.Embodiment'} />,
+    ],
+    [
+      <NameCell name={'UK-HYPROOF-0004'} date={'2021/01/12, 22:50h'} />,
+      '1.0 MWh',
+      '4.2 MWh',
+      '45g CO2e',
+      <StatusCell
+        Icon={CertificateRevokedIcon}
+        color={'red'}
+        status={'Revoked'}
+      />,
+    ],
+    [
+      <NameCell name={'UK-HYPROOF-0005'} date={'2024/02/22, 22:50h'} />,
+      '0.5 MWh',
+      '4.2 MWh',
+      '45g CO2e',
+      <StatusCell Icon={CertificateIssueIcon} status={'issued'} />,
+    ],
+    [
+      <NameCell name={'UK-HYPROOF-0006'} date={'2024/02/02, 22:50h'} />,
+      '2.0 MWh',
+      '4.2 MWh',
+      '45g CO2e',
+      <StatusCell
+        Icon={CertificateRevokedIcon}
+        color={'red'}
+        status={'Revoked'}
+      />,
+    ],
   ],
 }
--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -11,39 +11,83 @@ export interface TableProps {
 }
 
 type RowValue = React.ReactNode | number | string | null | undefined
-const defaultStyle = `color: var(--Black, #000);font-family: Roboto Mono;font-size: 16px;font-style: normal;text-align: left;font-weight: 400;line-height: normal;`
+const defaultStyle = `
+  color: var(--Black, #000);
+  font-family: Roboto Mono;
+  font-size: 16px;
+  font-style: normal;
+  text-align: left;
+  font-weight: 400;
+  line-height: normal;
+  text-decoration-line: underline;
+`
+
+const hyproof = `
+  border-bottom: solid 1px #FFF;
+  text-align: left;
+  padding: 2px 5px;
+  border-left: solid 1px #FFF;
+  background-color: #27847A;
+  color: #FFF;
+
+font-family: Roboto;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: 26x; /* 171.429% */
+`
 
 const Table: React.FC<React.PropsWithChildren<TableProps>> = (props) => (
-  <Wrapper {...props}>
-    <TR key={Math.random().toString()} style={props.styles?.tr}>
-      {props.headers.map((header: string) => (
-        <TH key={Math.random().toString()} style={props.styles?.th || {}}>
-          {header}
-        </TH>
-      ))}
-    </TR>
-    {props.rows.map((row: RowValue[]) => (
+  <Background>
+    <Wrapper {...props}>
       <TR key={Math.random().toString()} style={props.styles?.tr}>
-        {row.map((value: RowValue) => (
-          <TD key={Math.random().toString()}>{value}</TD>
+        {props.headers.map((header: string) => (
+          <TH key={Math.random().toString()} style={props.styles?.th || {}}>
+            {header}
+          </TH>
         ))}
       </TR>
-    ))}
-  </Wrapper>
+      {props.rows.map((row: RowValue[]) => (
+        <TR key={Math.random().toString()} style={props.styles?.tr}>
+          {row.map((value: RowValue) => (
+            <TD key={Math.random().toString()}>{value}</TD>
+          ))}
+        </TR>
+      ))}
+    </Wrapper>
+  </Background>
 )
 
 const TH = styled('th')`
-  ${defaultStyle}
-  text-decoration-line: underline;
+  ${hyproof}
 `
 const TD = styled('td')`
-  ${defaultStyle}
+  color: #1a1a1a;
+
+  font-family: Inter;
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 0px; /* 0% */
+  padding: 5px;
+  height: 60px;
+  text-align: center;
 `
-const TR = styled('tr')``
+const TR = styled('tr')`
+  background-color: #fff;
+  outline: thin solid #27847a;
+`
 
 const Wrapper = styled('table')<TableProps>`
   width: ${({ width }) => width || '100%'};
-  border-collapse: collapse;
+  border-collapse: separate;
+  border-spacing: 0 10px;
+`
+
+const Background = styled('div')`
+  background-color: #27847a;
+  padding: 20px;
+  height: 1200px;
 `
 
 export default Table
